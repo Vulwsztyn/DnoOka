4c9b648a23a741dff4fc26609a61786b00221a66
Luker <luker.plus@gmail.com>
2019-04-08 16:58:08 +0200

    data generation wip

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
 import numpy as np
-from helpers import poprawnoscWyniku,podzialNaCzesci
+import warnings
+warnings.filterwarnings("ignore")
 
+from skimage.io import imread, imsave
+
+def podzialNaCzesci(img,res,n=9):
+    if n%2==0:
+        raise ValueError("Wymiar maski ma być nieparzysty")
+    margines=n//2
+    czesci=[]
+    counter=0
+    result=[]
+    for i in range(margines,img.shape[0]-margines):
+        print(i-margines,' out of ',img.shape[0]-margines-margines)
+        for j in range(margines, img.shape[1] - margines):
+            czesc=[]
+            for a in range(-margines,n-margines,1):
+                wiersz=[]
+                for b in range(-margines,n-margines,1):
+                    wiersz.append(img[i+a][j+b])
+                czesc.append(wiersz)
+            if np.mean(czesc)>20:
+                result.append(res[i][j])
+                czesci.append(czesc.copy())
+        if (i-margines)%100==0:
+            np.savez_compressed('segments/01_h/'+str(counter),np.array(czesci))
+            np.savez_compressed('labels/01_h/' + str(counter), np.array(result))
+            counter = counter + 1
+            czesci=[]
+    np.savez_compressed('segments/01_h/' + str(counter), np.array(czesci))
+    np.savez_compressed('labels/01_h/' + str(counter), np.array(result))
+
+def toGrey(image,whichChannel):
+    for i in range(len(image)):
+        for j in range(len(image[whichChannel])):
+            image[i][j]=image[i][j][whichChannel]
+    return image
+
+image=imread('images/01_h.jpg')
+result=imread('results/01_h.tif')
+image=toGrey(image,1)
+podzialNaCzesci(image,result,9)
---
+++ test/keras_script.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from keras.models import Sequential
+from my_classes import DataGenerator
+
+# Parameters
+params = {'dim': (32,32,32),
+          'batch_size': 64,
+          'n_classes': 6,
+          'n_channels': 1,
+          'shuffle': True}
+
+# Datasets
+for i in range(24):
+    partition.append(i)
+labels = # Labels
+
+# Generators
+training_generator = DataGenerator(partition['train'], labels, **params)
+validation_generator = DataGenerator(partition['validation'], labels, **params)
+
+# Design model
+model = Sequential()
+[...] # Architecture
+model.compile()
+
+# Train model on dataset
+model.fit_generator(generator=training_generator,
+                    validation_data=validation_generator,
+                    use_multiprocessing=True,
+                    workers=6)
