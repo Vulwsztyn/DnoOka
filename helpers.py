686b0cd762b9e4e53de622fb703c2d702058f338
Vulwsztyn <artur.mostowski@student.put.poznan.pl>
2019-04-09 15:52:42 +0200

    add specificity & sensitivity

--- helpers.py
+++ helpers.py
@@ -4,12 +4,23 @@ import numpy as np
 def poprawnoscWyniku(wynik, wzor):
     if wynik.shape != wzor.shape:
         raise ValueError("Wzór i wynik mają różne wymiary")
-    poprawnosc = wzor.shape[0] * wzor.shape[1]
+    tp=fp=tn=fn=0
     for i in range(wzor.shape[0]):
         for j in range(wzor.shape[1]):
-            if wzor[i][j] != wynik[i][j]:
-                poprawnosc -= 1
-    return poprawnosc / (wzor.shape[0] * wzor.shape[1])
+            if wzor[i][j]==1:
+                if wynik[i][j] == 1:
+                    tp+=1
+                else:
+                    fn+=1
+            else:
+                if wynik[i][j]==1:
+                    fp+=1
+                else:
+                    fn+=1
+    accuracy=(tp+tn)/(tp+tn+fp+fn)
+    sensitivity = tp/(tp+fn)
+    specificity = tn/(tn+fp)
+    return accuracy,sensitivity,specificity
 
 def podzialNaCzesci(img,n=9):
     if n%2==0:
