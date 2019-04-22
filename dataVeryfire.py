fc603fb5748ce82a2bce409057ea0c121fd14e1a
Luker <luker.plus@gmail.com>
2019-04-22 20:14:41 +0200

    what a fucking mess

--- dataVeryfire.py
+++ dataVeryfire.py
@@ -5,29 +5,21 @@ from tensorflow import keras
 from skimage import exposure
 import matplotlib.pyplot as plt
 import os
+from numpy import genfromtxt
+import pandas as pd
 
 warnings.filterwarnings("ignore")
 from skimage.io import imread, imsave
 
 if __name__ == '__main__':
-    data=np.load('segments/01_dr/0.npz')['arr_0']
-    labels = np.load('labels/01_dr/0.npz')['arr_0']
-    for i in range(1,23):
-        tempS = np.load('segments/01_dr/'+str(i)+'.npz')['arr_0']
-        tempL = np.load('labels/01_dr/'+str(i)+'.npz')['arr_0']
-        np.concatenate((data,tempS))
-        np.concatenate((labels, tempL))
+    data = genfromtxt('params/01_dr.csv', delimiter=';',names=True)
 
-    names=('pixel value','average','mean','average without pixelvalue','labels average')
-    for i in range(5):
-        for j in range(i+1,5):
-                x = [(x[i]) for x in data]
-                if j==4:
-                    y = [(x[j] / 255) for x in data]
-                    # zapomniałem podzielić w oryginale, dalej już będzie podzielone
-                else:
-                    y = [(x[j]) for x in data]
-                plt.scatter(x, y, c=labels,s=0.7)
-                plt.xlabel(names[i])
-                plt.ylabel(names[j])
+    names=data.dtype.names
+    for i in names[:-1]:
+        for j in names[:-1]:
+                x = data[i]
+                y = data[j]
+                plt.scatter(x, y, c=data['dec'],s=0.7)
+                plt.xlabel(i)
+                plt.ylabel(j)
                 plt.show()
